1a4e4fea9045c44f9e226338727c7f2e574d4e0e
Carl A. Fahlstrom <carl.fahlstrom@gmail.com>
2017-01-30 11:26:05 -0700

    Added an express server that does not serve anything yet.

--- server/server.js
+++ server/server.js
@@ -3,5 +3,17 @@
 // Use the express framework for server
 
 var express = require('express');
+// var http = require('http');
 
-console.log('Hello World');
+// http.createServer(function (req, res) {
+  // res.writeHead(200, {'Content-Type': 'text/plain'});
+  // res.end("Hello World");
+// }).listen(3000);
+
+var app = express();
+
+app.listen(3000, function () {
+  console.log("The server is running on port 3000.");
+});
+
+// console.log('Server is up and running on port 3000');
